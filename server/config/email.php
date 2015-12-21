689df991e1bbbabf07f040f7dea544b550a2744d
Your NameKhang <khanghuynh92@gmail.com>
2015-12-21 15:35:11 +0700

    + added auth api (controllers/auth.php) for interacting angularjs by tank auth

--- server/config/email.php
+++ server/config/email.php
@@ -14,6 +14,14 @@ $config['mailtype'] = 'html';
 $config['charset'] = 'utf-8';
 $config['newline'] = "\r\n";
 
+$config['protocol'] = 'smtp';
+$config['charset'] = 'iso-8859-1';
+$config['mailtype'] = 'html';
 
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_port'] = '465';
+
+$config['smtp_user'] = 'Your Email';
+$config['smtp_pass'] = 'Your Password';
 /* End of file email.php */
 /* Location: ./application/config/email.php */
